314750f1d0d204a06406641705c647c4bfba97ab
Hack Reactor Students <willgester@gmail.com>
2014-01-17 16:53:02 -0800

    can drag user object

--- watchout.js
+++ watchout.js
@@ -4,12 +4,50 @@ $(document).ready(function(){
 
   var gameBoard = container.selectAll('svg').data(["svg"]).enter().append('svg');
 
-  var enemies = gameBoard.selectAll('circle').data(d3.range(50)).enter().append('circle')
+  var enemies = gameBoard
+      .selectAll('circle')
+      .data(d3.range(50))
+      .enter()
+      .append('circle')
+      // .append('pattern')
+      // .append('image')
+      // .attr('xlink:href', './img/Shuriken.png')
+      // .attr('x', 0)
+      // .attr('y', 0)
+      // .attr('width', 20)
+      // .attr('height', 20);
+
+  var userObject = container
+    .selectAll('svg')
+    .selectAll('rect')
+    .data(['user'])
+    .enter()
+    .append('rect')
+    .attr('fill', 'red')
+    .attr('height', 15)
+    .attr('width', 15)
+    .attr('x', 300)
+    .attr('y', 200);
+    // .behavior.drag();
+
+  var drag1 = d3.behavior.drag()
+    .on("drag", function(d,i) {
+        resetUser(d3.mouse(this));
+    });
+
+  var resetUser = function(coordinates){
+    var x = coordinates[0];
+    var y = coordinates[1];
+    userObject.attr('x', x).attr('y', y);
+  }
+
+  drag1.call(userObject);
 
   var setEnemies = function(){
 
-    enemies.data(_.map(d3.range(50), function(){return [Math.random()*600, Math.random()*400];}))
+    gameBoard.selectAll('circle').data(_.map(d3.range(50), function(){return [Math.random()*600, Math.random()*400];}))
       .transition()
+      // .style('opacity', '0')
       .duration(1000)
       .attr('cx', function(datum){return datum[0];}) // Might need jquery
       .attr('cy', function(datum){return datum[1];})
